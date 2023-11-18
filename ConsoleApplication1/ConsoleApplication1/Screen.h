817b18273a9f4b91cee2bd7a080b8f42c502a66f
duonghieu2907 <duonghieu2907@gmail.com>
2023-11-19 01:42:06 +0700

    Update on front end
    - Using inheritance

--- ConsoleApplication1/ConsoleApplication1/Screen.h
+++ ConsoleApplication1/ConsoleApplication1/Screen.h
@@ -1,14 +1,108 @@
-#pragma once
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <iostream>
+#include <vector>
 #include "SFML/Graphics.hpp"
+#include "Textbox.h"
+#include "Button.h"
+
+enum class ScreenState {
+    MainScreen,
+    //GamePlayScreen,
+    SettingScreen
+};
 
 //ANY screen will be inheritance from this abstract base screen.
-class Screen 
-{
+class Screen {
 public:
-    virtual void handleEvent(sf::Event event, sf::RenderWindow& window, bool& endScreen);
-    virtual void update(sf::RenderWindow& window);
-    virtual void render(sf::RenderWindow& window);
-    virtual void setEndScreen(bool value);
-private: // my set to protected if need
+    Screen(sf::RenderWindow& window) : isEndScreen(false) { initFont(); }
+    ~Screen() {}
+
+    virtual void handleEvent(sf::Event event, sf::RenderWindow& window, ScreenState& currentScreen, bool& endScreen) {}
+    virtual void update(sf::RenderWindow& window) {}
+    virtual void render(sf::RenderWindow& window) {}
+    void setEndScreen(bool value) { isEndScreen = value; }
+
+protected:
+    virtual void initBackground(sf::RenderWindow& window) {}
+    void initFont();
+
+protected: // my set to protected if need
+    sf::Font font;
     bool isEndScreen;
+
 };
+
+class MainScreen : public Screen {
+public:
+    MainScreen(sf::RenderWindow& window);
+    ~MainScreen() {}
+
+    void handleEvent(sf::Event event, sf::RenderWindow& window, ScreenState& currentScreen, bool& endScreen) override;
+    void update(sf::RenderWindow& window) override;
+    void render(sf::RenderWindow& window) override;
+
+private:
+    void initBackground(sf::RenderWindow& window) override;
+    void initPlayButton();
+    void initInstructionButton();
+    void initSettingButton();
+    void initExitButton();
+
+private:
+    sf::Texture mainScreenTex;
+    sf::Sprite mainScreen;
+
+    sf::Texture playButtonTex;
+    ButtonCustom playButton;
+
+    sf::Texture intButtonTex;
+    ButtonCustom instructionButton;
+
+    sf::Texture settingButtonTex;
+    ButtonCustom settingButton;
+
+    sf::Texture exitButtonTex;
+    ButtonCustom exitButton;
+};
+
+class SettingScreen : public Screen {
+public:
+    SettingScreen(sf::RenderWindow& window);
+    ~SettingScreen() {}
+
+public:
+    void handleEvent(sf::Event event, sf::RenderWindow& window, ScreenState& currentScreen, bool& endScreen) override;
+    void update(sf::RenderWindow& window) override;
+    void render(sf::RenderWindow& window) override;
+
+private:
+    void initBackground(sf::RenderWindow& window);
+    void initBackButton();
+    void initSaveButton();
+
+private:
+    sf::Texture settingScreenTex;
+    sf::Sprite settingScreen;
+
+    sf::Texture saveButtonTex;
+    ButtonCustom saveButton;
+
+    sf::Texture backButtonTex;
+    ButtonCustom backButton;
+
+};
+
+class ScreenControl {
+public:
+    ScreenControl(sf::RenderWindow& window);
+    ~ScreenControl();
+
+    Screen* getScreen(ScreenState state);
+
+private:
+    std::vector<Screen*> screens;
+};
+
+#endif
--- ConsoleApplication1/ConsoleApplication1/main.h
+++ ConsoleApplication1/ConsoleApplication1/main.h
@@ -3,74 +3,36 @@
 #pragma once
 #include <iostream>
 #include "SFML/Graphics.hpp"
-#include "Textbox.h"
-#include "Button.h"
-#include "Settings.h"
+#include "Screen.h"
 
 static const unsigned int SCREEN_HEIGHT = 810;
 static const unsigned int SCREEN_WIDTH = 1440;
 
 class Application {
 public:
-    enum class ScreenState {
-        MainScreen,
-        GamePlayScreen,
-        SettingScreen
-    };
-public:
     Application();
     ~Application();
     void run();
+
 public:
     void handleEvent();
     void update();
     void render();
+
+private:
+    void initWindow();
+
 private:
     sf::VideoMode videoMode;
     sf::RenderWindow window;
 
-    sf::Texture mainScreenTex;
-    sf::Sprite mainScreen;
-
-    sf::Texture playButtonTex;
-
-    sf::Texture intButtonTex;
-
-    sf::Texture settingButtonTex;
-
-    sf::Texture exitButtonTex;
-
-    sf::Texture saveButtonTex;
-
-    sf::Font font;
-
-    ScreenState currentScreen;
-
     sf::Event event;
 
-    ButtonCustom playButton;
-    ButtonCustom instructionButton;
-    ButtonCustom settingButton;
-    ButtonCustom exitButton;
+    ScreenState curScreenState;
 
-    Button continueButton;
-    Button newGameButton;
-    Button backButton;
-    ButtonCustom saveButton;
+    ScreenControl screenCtrl;
+    Screen* curScreen;
 
-    Settings* settings;
-private:
-    void initWindow();
-    void initBackground();
-    void initFont();
-    void initPlayButton();
-    void initInstructionButton();
-    void initSettingButton();
-    void initExitButton();
-    void initContinueButton();
-    void initNewGameButton();
-    void initBackButton();
-    void initSaveButton();
 };
 
 #endif
